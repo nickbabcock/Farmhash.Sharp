db297caf7a343d7bf93b7214871191de02dc434b
Nick Babcock <nbabcock19@hotmail.com>
2015-11-10 16:35:05 -0500

    Initial commit

---
+++ src/Farmhash.Sharp/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Farmhash.Sharp")>]
+[<assembly: AssemblyProductAttribute("Farmhash.Sharp")>]
+[<assembly: AssemblyDescriptionAttribute("Port of Google's farm hash algorithm")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ tests/Farmhash.Sharp.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module Farmhash.Sharp.Tests
+
+open Farmhash.Sharp
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
