d34f7cbb7847b8ece06a6aa9d1ad5f6105107e61
Nick Babcock <nbabcock19@hotmail.com>
2015-11-10 23:26:50 -0500

    Implement 32bit hashing

--- src/Farmhash.Sharp/Class1.cs
+++ src/Farmhash.Sharp/Class1.cs
@@ -1,15 +1,175 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable SuggestVarOrType_BuiltInTypes
 namespace Farmhash.Sharp
 {
     public class Class1
     {
-        public static int hello() {
-            return 42;
+        // Some primes between 2^63 and 2^64 for various uses.
+        const ulong k0 = 0xc3a5c85c97cb3127U;
+        const ulong k1 = 0xb492b66fbe98f273U;
+        const ulong k2 = 0x9ae16a3b2f90404fU;
+
+        // Magic numbers for 32-bit hashing.  Copied from Murmur3.
+        const uint c1 = 0xcc9e2d51;
+        const uint c2 = 0x1b873593;
+
+        private static uint Rotate32(uint val, int shift)
+        {
+            return shift == 0 ? val : (val >> shift) | (val << (32 - shift));
+        }
+
+        private ulong Rotate64(ulong val, int shift)
+        {
+            return shift == 0 ? val : (val >> shift) | (val << (64 - shift));
+        }
+
+        public static uint Rotate(uint val, int shift)
+        {
+            return Rotate32(val, shift);
+        }
+
+        private static unsafe ulong Fetch64(byte* p)
+        {
+            return *(ulong*) p;
+        }
+
+        private static unsafe uint Fetch32(byte* p)
+        {
+            return *(uint*)p;
+        }
+
+        private static unsafe uint Fetch(byte* p)
+        {
+            return Fetch32(p);
+        }
+
+        private static uint fmix(uint h)
+        {
+            h ^= h >> 16;
+            h *= 0x85ebca6b;
+            h ^= h >> 13;
+            h *= 0xc2b2ae35;
+            h ^= h >> 16;
+            return h;
+        }
+
+        private static unsafe uint Hash32Len0to4(byte* s, int len)
+        {
+            uint b = 0;
+            uint c = 9;
+            for (int i = 0; i < len; i++)
+            {
+                sbyte v = (sbyte) s[i];
+                b = (uint) (b * c1 + v);
+                c ^= b;
+            }
+            return fmix(Mur(b, Mur((uint) len, c)));
+        }
+
+        private static uint Mur(uint a, uint h)
+        {
+            // Helper from Murmur3 for combining two 32-bit values.
+            a *= c1;
+            a = Rotate32(a, 17);
+            a *= c2;
+            h ^= a;
+            h = Rotate32(h, 19);
+            return h * 5 + 0xe6546b64;
+        }
+
+        private static unsafe uint Hash32Len13to24(byte* s, int len, uint seed = 0) {
+            uint a = Fetch(s - 4 + (len >> 1));
+            uint b = Fetch(s + 4);
+            uint c = Fetch(s + len - 8);
+            uint d = Fetch(s + (len >> 1));
+            uint e = Fetch(s);
+            uint f = Fetch(s + len - 4);
+            uint h = (uint) (d * c1 + len + seed);
+            a = Rotate(a, 12) + f;
+            h = Mur(c, h) + a;
+            a = Rotate(a, 3) + c;
+            h = Mur(e, h) + a;
+            a = Rotate(a + f, 12) + d;
+            h = Mur(b ^ seed, h) + a;
+            return fmix(h);
+        }
+
+        private static unsafe uint Hash32Len5to12(byte* s, int len, uint seed = 0)
+        {
+            uint a = (uint) len, b = (uint) (len * 5), c = 9, d = b + seed;
+            a += Fetch(s);
+            b += Fetch(s + len - 4);
+            c += Fetch(s + ((len >> 1) & 4));
+            return fmix(seed ^ Mur(c, Mur(b, Mur(a, d))));
+        }
+
+        private static unsafe uint Hash32(byte* s, int len)
+        {
+            if (len <= 24)
+            {
+                return len <= 12 ?
+                    (len <= 4 ? Hash32Len0to4(s, len) : Hash32Len5to12(s, len)) :
+                    Hash32Len13to24(s, len);
+            }
+
+            uint h = (uint) len, g = (uint) (c1 * len), f = g;
+            uint a0 = Rotate(Fetch(s + len - 4) * c1, 17) * c2;
+            uint a1 = Rotate(Fetch(s + len - 8) * c1, 17) * c2;
+            uint a2 = Rotate(Fetch(s + len - 16) * c1, 17) * c2;
+            uint a3 = Rotate(Fetch(s + len - 12) * c1, 17) * c2;
+            uint a4 = Rotate(Fetch(s + len - 20) * c1, 17) * c2;
+            h ^= a0;
+            h = Rotate(h, 19);
+            h = h * 5 + 0xe6546b64;
+            h ^= a2;
+            h = Rotate(h, 19);
+            h = h * 5 + 0xe6546b64;
+            g ^= a1;
+            g = Rotate(g, 19);
+            g = g * 5 + 0xe6546b64;
+            g ^= a3;
+            g = Rotate(g, 19);
+            g = g * 5 + 0xe6546b64;
+            f += a4;
+            f = Rotate(f, 19) + 113;
+            int iters = (len - 1) / 20;
+            do
+            {
+                uint a = Fetch(s);
+                uint b = Fetch(s + 4);
+                uint c = Fetch(s + 8);
+                uint d = Fetch(s + 12);
+                uint e = Fetch(s + 16);
+                h += a;
+                g += b;
+                f += c;
+                h = Mur(d, h) + e;
+                g = Mur(c, g) + a;
+                f = Mur(b + e * c1, f) + d;
+                f += g;
+                g += f;
+                s += 20;
+            } while (--iters != 0);
+            g = Rotate(g, 11) * c1;
+            g = Rotate(g, 17) * c1;
+            f = Rotate(f, 11) * c1;
+            f = Rotate(f, 17) * c1;
+            h = Rotate(h + g, 19);
+            h = h * 5 + 0xe6546b64;
+            h = Rotate(h, 17) * c1;
+            h = Rotate(h + f, 19);
+            h = h * 5 + 0xe6546b64;
+            h = Rotate(h, 17) * c1;
+            return h;
+        }
+
+
+        public static unsafe uint Hash32(byte[] s, int len)
+        {
+            fixed (byte* buf = s)
+            {
+                return Hash32(buf, len);
+            }
         }
     }
 }
--- tests/Farmhash.Sharp.Tests/Tests.fs
+++ tests/Farmhash.Sharp.Tests/Tests.fs
@@ -1,10 +1,82 @@
 module Farmhash.Sharp.Tests
 
 open Farmhash.Sharp
+open System.Text
 open NUnit.Framework
 
+let (``hash32 len 0 to 4 cases``:obj[][]) = [|
+  [| ""; 0xdc56d17au |]
+  [| "a"; 0x3c973d4du |]
+  [| "ab"; 0x417330fdu |]
+  [| "abc"; 0x2f635ec7u |]
+  [| "abcd"; 0x98b51e95u |]
+  [| "hi"; 0xf2311502u |]
+|]
+
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Class1.hello()
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+[<TestCaseSource("hash32 len 0 to 4 cases")>]
+let ``hash32 len 0 to 4`` (str:string) expected =
+  let bytes = Encoding.ASCII.GetBytes(str)
+  let actual = Class1.Hash32(bytes, bytes.Length)
+  Assert.AreEqual(actual, expected)
+
+
+let (``hash32 len 5 to 12 cases``:obj[][]) = [|
+  [| "abcde"; 0xa3f366acu |]
+  [| "abcdef"; 0x0f813aa4u |]
+  [| "abcdefg"; 0x21deb6d7u |]
+  [| "abcdefgh"; 0xfd7ec8b9u |]
+  [| "abcdefghi"; 0x6f98dc86u |]
+  [| "abcdefghij"; 0xf2669361u |]
+  [| "hello world"; 0x19a7581au |]
+|]
+
+[<Test>]
+[<TestCaseSource("hash32 len 5 to 12 cases")>]
+let ``hash32 len 5 to 12`` (str:string) expected =
+  let bytes = Encoding.ASCII.GetBytes(str)
+  let actual = Class1.Hash32(bytes, bytes.Length)
+  Assert.AreEqual(actual, expected)
+
+let (``hash32 len 13 to 24 cases``:obj[][]) = [|
+  [| "fred@example.com"; 0x7acdc357u |]
+  [| "lee@lmmrtech.com"; 0xaf0a30feu |]
+  [| "docklandsman@gmail.com"; 0x5d8cdbf4u |]
+  [| "size:  a.out:  bad magic"; 0xc6246b8du |]
+|]
+
+[<Test>]
+[<TestCaseSource("hash32 len 13 to 24 cases")>]
+let ``hash32 len 13 to 24`` (str:string) expected =
+  let bytes = Encoding.ASCII.GetBytes(str)
+  let actual = Class1.Hash32(bytes, bytes.Length)
+  Assert.AreEqual(actual, expected)
+
+let (``hash32 larger cases``:obj[][]) = [|
+  [| "Go is a tool for managing Go source code.Usage:	go command [arguments]The commands are:    build       compile packages and dependencies    clean       remove object files    env         print Go environment information    fix         run go tool fix on packages    fmt         run gofmt on package sources    generate    generate Go files by processing source    get         download and install packages and dependencies    install     compile and install packages and dependencies    list        list packages    run         compile and run Go program    test        test packages    tool        run specified go tool    version     print Go version    vet         run go tool vet on packagesUse go help [command] for more information about a command.Additional help topics:    c           calling between Go and C    filetype    file types    gopath      GOPATH environment variable    importpath  import path syntax    packages    description of package lists    testflag    description of testing flags    testfunc    description of testing functionsUse go help [topic] for more information about that topic."; 0x9c8f96f3u |]
+  [| "Discard medicine more than two years old."; 0xe273108fu |]
+  [| "He who has a shady past knows that nice guys finish last."; 0xf585dfc4u |]
+  [| "I wouldn't marry him with a ten foot pole."; 0x363394d1u |]
+  [| "Free! Free!/A trip/to Mars/for 900/empty jars/Burma Shave"; 0x7613810fu |]
+  [| "The days of the digital watch are numbered.  -Tom Stoppard"; 0x2cc30bb7u |]
+  [| "Nepal premier won't resign."; 0x322984d9u |]
+  [| "For every action there is an equal and opposite government program."; 0xa5812ac8u |]
+  [| "His money is twice tainted: 'taint yours and 'taint mine."; 0x1090d244u |]
+  [| "There is no reason for any individual to have a computer in their home. -Ken Olsen, 1977"; 0xff16c9e6u |]
+  [| "It's a tiny change to the code and not completely disgusting. - Bob Manchek"; 0xcc3d0ff2u |]
+  [| "The major problem is with sendmail.  -Mark Horton"; 0xd225e92eu |]
+  [| "Give me a rock, paper and scissors and I will move the world.  CCFestoon"; 0x1b8db5d0u |]
+  [| "If the enemy is within range, then so are you."; 0x4fda5f07u |]
+  [| "It's well we cannot hear the screams/That we create in others' dreams."; 0x2e18e880u |]
+  [| "You remind me of a TV show, but that's all right: I watch it anyway."; 0xd07de88fu |]
+  [| "C is as portable as Stonehedge!!"; 0x221694e4u |]
+  [| "Even if I could be Shakespeare, I think I should still choose to be Faraday. - A. Huxley"; 0xe2053c2cu |]
+  [| "The fugacity of a constituent in a mixture of gases at a given temperature is proportional to its mole fraction.  Lewis-Randall Rule"; 0x11c493bbu |]
+  [| "How can you write a big system without C++?  -Paul Glick"; 0x0819a4e8u |]
+|]
+[<Test>]
+[<TestCaseSource("hash32 larger cases")>]
+let ``hash32 larger`` (str:string) expected =
+  let bytes = Encoding.ASCII.GetBytes(str)
+  let actual = Class1.Hash32(bytes, bytes.Length)
+  Assert.AreEqual(actual, expected)
