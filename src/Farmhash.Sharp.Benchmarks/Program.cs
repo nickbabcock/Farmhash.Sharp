cfacc6a28c34b9b370a7609ec858f955f5626cbb
Nick Babcock <nbabcock19@hotmail.com>
2015-11-11 21:17:47 -0500

    Benchmarks with other algorithms

--- src/Farmhash.Sharp.Benchmarks/Program.cs
+++ src/Farmhash.Sharp.Benchmarks/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Data.HashFunction;
 using System.Diagnostics;
+using System.Security.Cryptography;
 using System.Text;
 using xxHashSharp;
 
@@ -12,12 +14,16 @@ namespace Farmhash.Sharp.Benchmarks
             return Encoding.ASCII.GetBytes(str);
         }
 
-        private static readonly byte[] Smallest = StrToBytes("doge");
-        private static readonly byte[] Smaller = StrToBytes("hello world");
-        private static readonly byte[] Small = StrToBytes("Hello I'm Farmhash.Sharp");
-        private static readonly byte[] Medium = StrToBytes(new string('.', 100));
-        private static readonly byte[] Large = StrToBytes(new string('.', 1000));
-        private static readonly byte[] Largest = StrToBytes(new string('.', 10000));
+        private const string Smallest = "doge";
+        private const string Smaller = "hello world";
+        private const string Small = "Hello I'm Farmhash.Sharp";
+        private static readonly string Medium = new string('.', 100);
+        private static readonly string Large = new string('.', 1000);
+        private static readonly string Largest = new string('.', 10000);
+
+        private static readonly MD5 md5 = MD5.Create();
+        private static readonly System.Data.HashFunction.CityHash hcity = new System.Data.HashFunction.CityHash(32);
+        private static readonly SpookyHashV2 Spooky = new SpookyHashV2(32);
 
         static void Main()
         {
@@ -25,9 +31,50 @@ namespace Farmhash.Sharp.Benchmarks
             Console.WriteLine("Name\tSmallest\tSmaller\tSmall\tMedium\tLarge\tLargest");
             ProfileSuite("Farmhash", 10000, bytes => Farmhash.Hash32(bytes, bytes.Length));
             ProfileSuite("xxHashSharp", 10000, bytes => xxHash.CalculateHash(bytes));
+            ProfileSuite("CityHash.Net", 1000, str => CityHash.CityHash.CityHash32(str));
+            ProfileSuite("String hashCode", 10000, (string str) => str.GetHashCode());
+            ProfileSuite("HashFunction CityHash", 10000, (byte[] bytes) => hcity.ComputeHash(bytes));
+            ProfileSuite("HashFunction Spooky", 1000, (byte[] bytes) => Spooky.ComputeHash(bytes));
+            ProfileSuite("MD5sum", 100, bytes => md5.ComputeHash(bytes));
         }
 
         private static void ProfileSuite(string description, int iterations, Action<byte[]> calc)
+        {
+            double smallest, smaller, small, medium, large, largest;
+            {
+                byte[] bytes = StrToBytes(Smallest);
+                smallest = Profile(iterations, () => calc(bytes));
+            }
+
+            {
+                byte[] bytes = StrToBytes(Smaller);
+                smaller = Profile(iterations, () => calc(bytes));
+            }
+
+            {
+                byte[] bytes = StrToBytes(Small);
+                small = Profile(iterations, () => calc(bytes));
+            }
+
+            {
+                byte[] bytes = StrToBytes(Medium);
+                medium = Profile(iterations, () => calc(bytes));
+            }
+
+            {
+                byte[] bytes = StrToBytes(Large);
+                large = Profile(iterations, () => calc(bytes));
+            }
+
+            {
+                byte[] bytes = StrToBytes(Largest);
+                largest = Profile(iterations, () => calc(bytes));
+            }
+
+            Console.WriteLine("{0}\t{1:N}\t{2:N}\t{3:N}\t{4:N}\t{5:N}\t{6:N}", description, smallest, smaller, small, medium, large, largest);
+        }
+
+        private static void ProfileSuite(string description, int iterations, Action<string> calc)
         {
             var smallest = Profile(iterations, () => calc(Smallest));
             var smaller = Profile(iterations, () => calc(Smaller));
