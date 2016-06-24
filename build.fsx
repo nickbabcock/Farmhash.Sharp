90ee92c6472a12afb76c037fd2e7b54d12327a74
Nick Babcock <nbabcock19@hotmail.com>
2016-06-23 20:09:39 -0400

    Update to NUnit 3

--- build.fsx
+++ build.fsx
@@ -8,6 +8,7 @@ open Fake.Git
 open Fake.AssemblyInfoFile
 open Fake.ReleaseNotesHelper
 open Fake.UserInputHelper
+open Fake.Testing
 open System
 open System.IO
 #if MONO
@@ -137,11 +138,8 @@ Target "Build" (fun _ ->
 
 Target "RunTests" (fun _ ->
     !! testAssemblies
-    |> NUnit (fun p ->
-        { p with
-            DisableShadowCopy = true
-            TimeOut = TimeSpan.FromMinutes 20.
-            OutputFile = "TestResults.xml" })
+    |> NUnit3 (fun p ->
+        { p with TimeOut = TimeSpan.FromMinutes 20. })
 )
 
 #if MONO
--- src/Farmhash.Sharp.Benchmarks/Properties/AssemblyInfo.cs
+++ src/Farmhash.Sharp.Benchmarks/Properties/AssemblyInfo.cs
@@ -9,5 +9,6 @@ using System.Reflection;
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "0.2";
+        internal const string InformationalVersion = "0.2";
     }
 }
