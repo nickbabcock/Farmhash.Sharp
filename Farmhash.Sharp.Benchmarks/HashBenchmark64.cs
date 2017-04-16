e24dd417cea721b1b20dd5d669f1ef5eef17586e
Nick Babcock <nbabcock19@hotmail.com>
2017-04-15 21:24:58 -0400

    Add start of 64bit benchmark

---
+++ Farmhash.Sharp.Benchmarks/HashBenchmark64.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using System.Text;
+
+namespace Farmhash.Sharp.Benchmarks
+{
+#if CORE
+    [CoreJob]
+#elif MONO
+    [MonoJob]
+#else
+    [ClrJob]
+    [LegacyJitX86Job, LegacyJitX64Job, RyuJitX64Job]
+#endif
+    public class HashBenchmark64
+    {
+        private byte[] data;
+        private string dataStr;
+
+        [Setup]
+        public void SetupData()
+        {
+            dataStr = new string('.', PayloadLength);
+            data = Encoding.ASCII.GetBytes(dataStr);
+        }
+
+
+        [Params(4, 11, 25, 100, 1000, 10000)]
+        public int PayloadLength { get; set; }
+
+        [Benchmark]
+        public ulong FarmHash() => Farmhash.Hash64(data, data.Length);
+    }
+}
