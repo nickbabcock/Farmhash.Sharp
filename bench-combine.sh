795838ce80da218ec5f12d1a9c03c01386547d39
Nick Babcock <nbabcock19@hotmail.com>
2017-04-20 22:14:16 -0400

    Update the combine benchmark script

--- bench-combine.sh
+++ bench-combine.sh
@@ -1,20 +1,5 @@
 #!/bin/bash -e
 
-mlr --icsv --ocsv --rs lf put '$OutputBits = "32"' HashBenchmark32-report.csv > benchmarks.csv
-mlr --icsv --ocsv --rs lf put '$OutputBits = "64"' HashBenchmark64-report.csv | \
-    xsv cat rows - benchmarks.csv | \
-    sponge benchmarks.csv
-
-mlr --icsv --ocsv --rs lf put '$OutputBits = "32"' ../BenchmarkDotNet.Artifacts/results/HashBenchmark32-report.csv | \
-    xsv cat rows - benchmarks.csv | \
-    sponge benchmarks.csv
-mlr --icsv --ocsv --rs lf put '$OutputBits = "64"' ../BenchmarkDotNet.Artifacts/results/HashBenchmark64-report.csv | \
-    xsv cat rows - benchmarks.csv | \
-    sponge benchmarks.csv
-
-mlr --icsv --ocsv put '$OutputBits = "32"' net46-results/HashBenchmark32-report.csv | \
-    xsv cat rows - benchmarks.csv | \
-    sponge benchmarks.csv
-mlr --icsv --ocsv put '$OutputBits = "64"' net46-results/HashBenchmark64-report.csv | \
-    xsv cat rows - benchmarks.csv | \
-    sponge benchmarks.csv
+xsv cat rows core-HashBenchmark32-report.csv core-HashBenchmark64-report.csv \
+             mono-HashBenchmark32-report.csv mono-HashBenchmark64-report.csv \
+             clr-HashBenchmark32-report.csv clr-HashBenchmark64-report.csv > benchmarks.csv
