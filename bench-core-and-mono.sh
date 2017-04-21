e66e67d2064a3ee11612b41c7192df1bf7477372
Nick Babcock <nbabcock19@hotmail.com>
2017-04-20 22:09:30 -0400

    Prefer bench script to be ran as sudo

--- bench-core-and-mono.sh
+++ bench-core-and-mono.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -e
 
-dotnet restore
-dotnet restore -r net46
+sudo -u $USER dotnet restore
+sudo -u $USER dotnet restore -r net46
 
-sudo dotnet run -c Release -f netcoreapp1.1 -p \
+dotnet run -c Release -f netcoreapp1.1 -p \
     Farmhash.Sharp.Benchmarks/Farmhash.Sharp.Benchmarks.csproj
 
-cp -r BenchmarkDotNet.Artifacts BenchmarkDotNet.Artifacts-temp
+sudo -u $USER cp -r BenchmarkDotNet.Artifacts BenchmarkDotNet.Artifacts-temp
+chown -R $USER.$USER BenchmarkDotNet.Artifacts-temp
 
 export FrameworkPathOverride=/usr/lib/mono/4.5/
-dotnet build -c Release -f net46 /property:DefineConstants=MONO \
+sudo -u $USER dotnet build -c Release -f net46 /property:DefineConstants=MONO \
     Farmhash.Sharp.Benchmarks/Farmhash.Sharp.Benchmarks.csproj
 
-sudo mono /home/nick/projects/Farmhash.Sharp/Farmhash.Sharp.Benchmarks/bin/Release/net46/Farmhash.Sharp.Benchmarks.exe
+mono /home/nick/projects/Farmhash.Sharp/Farmhash.Sharp.Benchmarks/bin/Release/net46/Farmhash.Sharp.Benchmarks.exe
+chown -R $USER.$USER BenchmarkDotNet.Artifacts
