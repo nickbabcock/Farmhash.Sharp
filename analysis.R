aa328347c6dad90d5b5dd909b90ae3639fd698c7
Nick Babcock <nbabcock19@hotmail.com>
2017-04-21 20:30:59 -0400

    Throughput by Jit Runtime

--- analysis.R
+++ analysis.R
@@ -12,7 +12,7 @@ df <- df %>%
 
 fm <- df %>% 
   filter((Method == 'FarmHash' | Method == 'Md5') &
-          Job == 'net-legacy-32bit' & Kind == '32bit hash') %>%
+           Job == 'net-legacy-32bit' & Kind == '32bit hash') %>%
   select(Method, PayloadLength, Median, StdErr, StdDev, Throughput)
 
 ggplot(fm, aes(as.factor(PayloadLength), Throughput)) +
@@ -20,3 +20,24 @@ ggplot(fm, aes(as.factor(PayloadLength), Throughput)) +
   labs(x='Payload (bytes)', y='Throughput MB/s') +
   ggtitle('Cryptographic vs Non-Cryptographic Hash Function',
           subtitle = "Using 32bit CLR with 32bit Farmhash as a baseline")
+
+clr_run <- df %>%
+  filter((Method == 'FarmHash') & Runtime == 'Clr' & Kind == '64bit hash') %>%
+  select(Method, Job, Runtime, PayloadLength, Median, StdErr, StdDev, Throughput)
+
+ggplot(clr_run, aes(as.factor(PayloadLength), Throughput)) +
+  geom_bar(aes(fill=Job), stat='identity', position='dodge') +
+  labs(x='Payload (bytes)', y='Throughput MB/s') + 
+  ggtitle("Throughput by Jit Runtime",
+          subtitle = "64bit Farmhash Performance on the Clr")
+
+
+mono_run <- df %>%
+  filter((Method == 'FarmHash') & Runtime == 'Mono' & Kind == '64bit hash') %>%
+  select(Method, Job, Runtime, PayloadLength, Median, StdErr, StdDev, Throughput)
+
+ggplot(mono_run, aes(as.factor(PayloadLength), Throughput)) +
+  geom_bar(aes(fill=Job), stat='identity', position='dodge') +
+  labs(x='Payload (bytes)', y='Throughput MB/s') + 
+  ggtitle("Throughput by Jit Runtime",
+          subtitle = "64bit Farmhash Performance on the Mono")
