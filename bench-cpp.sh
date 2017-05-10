da591ba32da47201aab27fbad7b765d2c2a6fc48
Nick Babcock <nbabcock19@hotmail.com>
2017-05-10 06:10:00 -0400

    Add echos to cpp benchmark for more context

--- bench-cpp.sh
+++ bench-cpp.sh
@@ -7,5 +7,10 @@ cd "$(dirname "$0")"
 cd Farmhash.Benchmarks
 sudo -u $THE_USER make all
 
+echo ""
+echo "Executing hardware accelerated farmhash"
 nice -n -20 ./benchmark-farmhash
+
+echo ""
+echo "Executing non-hardware accelerated farmhash"
 nice -n -20 ./benchmark-farmhash-no-ha
