65995d918955bf0a24123cd9e522389474e0c891
Nick Babcock <nbabcock19@hotmail.com>
2018-06-03 08:16:59 -0500

    Reference correct runtime in benchmark script

--- bench-core-and-mono.sh
+++ bench-core-and-mono.sh
@@ -6,14 +6,14 @@ THE_USER=${SUDO_USER:=$USER}
 sudo -u $THE_USER dotnet restore
 sudo -u $THE_USER dotnet restore -r net46
 
-dotnet run -c Release -f netcoreapp2.0 -p \
+dotnet run -c Release -f netcoreapp2.1 -p \
     Farmhash.Sharp.Benchmarks/Farmhash.Sharp.Benchmarks.csproj
 
 sudo -u $THE_USER cp -r BenchmarkDotNet.Artifacts BenchmarkDotNet.Artifacts-temp
 chown -R $THE_USER.$THE_USER BenchmarkDotNet.Artifacts-temp
 
 sudo -u $THE_USER FrameworkPathOverride=/usr/lib/mono/4.5-api/ \
-    dotnet build -c Release -f net46 /property:DefineConstants=MONO \
+    dotnet build -c Release -f net461 /property:DefineConstants=MONO \
     Farmhash.Sharp.Benchmarks/Farmhash.Sharp.Benchmarks.csproj
 
 mono Farmhash.Sharp.Benchmarks/bin/Release/net46/Farmhash.Sharp.Benchmarks.exe
