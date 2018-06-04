9a81e74dbd198b5d542c29b401271f0dd6f146ee
Nick Babcock <nbabcock19@hotmail.com>
2018-06-04 06:12:49 -0500

    Don't require sudo to run benchmarks

--- bench-core-and-mono.sh
+++ bench-core-and-mono.sh
@@ -1,17 +1,12 @@
 #!/bin/bash -e
-# This script must be invoked as a sudoer
-
-THE_USER=${SUDO_USER:=$USER}
 
 dotnet run -c Release -f netcoreapp2.1 -p \
     Farmhash.Sharp.Benchmarks/Farmhash.Sharp.Benchmarks.csproj
 
-sudo -u $THE_USER cp -r BenchmarkDotNet.Artifacts BenchmarkDotNet.Artifacts-temp
-chown -R $THE_USER.$THE_USER BenchmarkDotNet.Artifacts-temp
+cp -r BenchmarkDotNet.Artifacts BenchmarkDotNet.Artifacts-temp
 
-sudo -u $THE_USER FrameworkPathOverride=/usr/lib/mono/4.5-api/ \
+FrameworkPathOverride=/usr/lib/mono/4.5-api/ \
     dotnet build -c Release -f net461 /property:DefineConstants=MONO \
     Farmhash.Sharp.Benchmarks/Farmhash.Sharp.Benchmarks.csproj
 
 mono Farmhash.Sharp.Benchmarks/bin/Release/net46/Farmhash.Sharp.Benchmarks.exe
-chown -R $THE_USER.$THE_USER BenchmarkDotNet.Artifacts
