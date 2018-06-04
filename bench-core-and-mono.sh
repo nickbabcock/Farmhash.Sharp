ecc3946a304dcfba98dd0f871a77e4b2154f7ba9
Nick Babcock <nbabcock19@hotmail.com>
2018-06-04 16:52:43 -0500

    Fix benchmark directory for mono

--- bench-core-and-mono.sh
+++ bench-core-and-mono.sh
@@ -9,4 +9,4 @@ FrameworkPathOverride=/usr/lib/mono/4.5-api/ \
     dotnet build -c Release -f net461 /property:DefineConstants=MONO \
     Farmhash.Sharp.Benchmarks/Farmhash.Sharp.Benchmarks.csproj
 
-mono Farmhash.Sharp.Benchmarks/bin/Release/net46/Farmhash.Sharp.Benchmarks.exe
+mono Farmhash.Sharp.Benchmarks/bin/Release/net461/Farmhash.Sharp.Benchmarks.exe
